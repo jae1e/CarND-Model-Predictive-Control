1b79d9e7f322c5f435d8389726cfa739c4557612
jaeil <parkjaeil0108@gmail.com>
2017-07-11 20:57:36 +0900

    removed redundant code

--- src/main.cpp
+++ src/main.cpp
@@ -117,11 +117,6 @@ int main() {
           double cte = polyeval(coeffs, 0);
           double epsi = -atan(coeffs[1]);
 
-          // double cte_l = cte - 0.0 + v * sin(epsi) * dt_l;
-          // double epsi_l = psi + epsi - v/Lf * delta * dt_l;
-          mpc.prev_delta = delta;
-          mpc.prev_a = a;
-
           // solve
           Eigen::VectorXd state(6);
           // state << x_l, y_l, psi_l, v_l, cte, epsi;
